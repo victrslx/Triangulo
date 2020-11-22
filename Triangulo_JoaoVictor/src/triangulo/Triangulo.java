1071e0819fab974adb84c4c04a98f39c59406f3a
victrslx <70175963+victrslx@users.noreply.github.com>
2020-11-22 16:47:38 -0300

    Update Triangulo.java

--- Triangulo_JoaoVictor/src/triangulo/Triangulo.java
+++ Triangulo_JoaoVictor/src/triangulo/Triangulo.java
@@ -15,17 +15,26 @@ public class Triangulo {
         System.out.print("Insira o lado c do triângulo: ");
         c= entrada.nextDouble();
         
-        if ((a == 0) || (b == 0) || (c == 0)) {
-	System.out.println("Não pode ser Triangulo pois tem lado 0");
-	} if ((a + b > c) && (a + c > b) && (b + c > a)) {
-	System.out.println("Forma um triãngulo");
-	} if ((a == b) && (a == c) && (b == c)) {
-	System.out.println("Triagulo Equilátero");
-	} else if ((a == b) && (a != c) || (a == c) && (a != b) || (b == c) && (b != a)) {
-	System.out.println("Triangulo isósceles");
-	} else if ((a != b) && (a != c) && (b != c)) {
-	System.out.println("Triangulo escaleno");
-	}
+	     if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                System.out.println("Forma um triãngulo");
+
+                if ((a == b) && (a == c) && (b == c))
+                {
+                    System.out.println("Triagulo Equilátero");
+                }
+                else if ((a == b) && (a != c) || (a == c) && (a != b) || (b == c) && (b != a))
+                {
+                    System.out.println("Triangulo isósceles");
+                }
+                else if ((a != b) && (a != c) && (b != c))
+                {
+                    System.out.println("Triangulo escaleno");
+                }
+            }
+            else {
+                System.out.println("Não forma um triângulo, pois os valores são igauis a 0");            
+            }
     }
     
 }
